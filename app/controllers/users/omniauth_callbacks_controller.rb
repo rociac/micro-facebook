056aee86637016648f8765f83144b493cb007f8b
rociac <brsrkdev@gmail.com>
2019-11-12 16:17:51 -0600

    Fix linter errors

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
-    end
+  end
 
   def failure
     redirect_to root_path
