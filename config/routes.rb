cc44803538659bdf5ae72ea6b6b82d70dedf16ee
Adebola <onedebos@gmail.com>
2019-11-15 14:00:11 +0100

    Style homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :friends
   resources :friend_requests
   resources :posts do
-    resources :comments
+  resources :comments
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
