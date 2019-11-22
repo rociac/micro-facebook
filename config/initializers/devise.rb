4a5dc6906375d4c8a89b82ab969bc15507b98e8c
Rodolfo <brsrkdev@gmail.com>
2019-11-22 00:08:03 -0600

    Add files for facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,7 +296,8 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :facebook, "350684932413283", "7e9f113814e32f6d8a00061bb4a16901"
-
-
+config.omniauth :facebook,
+  ENV['FACEBOOK_APP_ID'],
+  ENV['FACEBOOK_APP_SECRET'],
+  callback_url:  "https://micro-social.herokuapp.com/users/auth/facebook/callback"
 end
